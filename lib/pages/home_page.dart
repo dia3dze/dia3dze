5577c78cdf9940711f7600ff87e38c92907b8d07
saba <saba2006.diasamidze@gmail.com>
2025-09-19 14:02:01 +0400

    rewrote main page content

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,14 +11,26 @@ class HomePage extends StatelessWidget {
       children: [
         Header(label: "Hello, my name is saba.", level: 1),
         Header(label: "I'm a computer Engineer.", level: 2),
+        Divider(),
         Header(label: "📍Georgia, tbilisi", level: 4),
+        Divider(),
+        Header(label: "Introduction", level: 4),
         Paragraph(
           text:
-              "Right now, I am a student with no professional experience, my skills are the result of my curiosity and dedication to exploring new topics. I enjoy learning through self-paced carriculum.",
+              "Welcome to my portfolio website. My name is Saba, and I’m an engineering student at Caucasus University. I don’t have professional work experience yet, but I’m improving my practical skills through hobby projects and personal practice.",
         ),
+        Paragraph(
+          text:
+              "This website is both a way to practice my programming skills and a place to document my most meaningful projects, focusing on the areas I’m most interested in pursuing further.",
+        ),
+        Divider(),
         Header(label: "Skills", level: 4),
         UnorderedList(
-          items: ["analytics", "programming & software", "operating systems"],
+          items: [
+            "Analytics & Data Visualization: R, Python, Excel",
+            "Programming & Software: Dart, Html/Css, Javascript",
+            "Operating Systems: Bash scripting, system configuration, system monitoring",
+          ],
         ),
       ],
     );
